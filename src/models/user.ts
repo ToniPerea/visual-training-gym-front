89e4c399fbb6c195ad372f66a092016182adf0db
ToniPerea <apepa31@gmail.com>
2022-06-25 22:07:14 +0200

    Creation of Register/User form #8

--- src/models/user.ts
+++ src/models/user.ts
@@ -5,6 +5,16 @@ import {
 
 export class User {
     /**
+   * User name
+   */
+  @autoserializeAs(() => String) name: string;
+
+  /**
+   * User age
+   */
+  @autoserializeAs(() => String) age: Date;
+
+    /**
    * User email
    */
   @autoserializeAs(() => String) email: string | null;
@@ -14,12 +24,33 @@ export class User {
    */
   @autoserializeAs(() => String) password: string | null;
 
+    /**
+   * User role
+   */
+  @autoserializeAs(() => String) role: string;
+
+  /**
+   * User status
+   */
+  @autoserializeAs(() => String) status: string;
+
 
   constructor(
+      name:string,
+      age: Date,
       email: string | null,
-      password: string | null
+      password: string | null,
+      role: string,
+      status: string
   ) {
+    this.name = name;
+    this.age = age;
     this.email = email;
     this.password = password;
+    this.role = role;
+    this.status = status;
   }
+
+
+
 }
