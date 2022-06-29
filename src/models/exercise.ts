2f7492839faf3f0feab3334c9a9129c9c837025e
ToniPerea <apepa31@gmail.com>
2022-06-29 16:13:53 +0200

    Implement service and exercise form #11

---
+++ src/models/exercise.ts
@@ -0,0 +1,24 @@
+import {autoserializeAs} from "dcerialize";
+
+
+export class Exercise {
+    /**
+     * User name
+     */
+    @autoserializeAs(() => String) name: string;
+
+    /**
+     * User age
+     */
+    @autoserializeAs(() => String) gif: string;
+
+    constructor(
+        name: string,
+        gif: string
+    ) {
+        this.name = name;
+        this.gif = gif;
+    }
+
+
+}
