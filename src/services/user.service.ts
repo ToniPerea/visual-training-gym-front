fa0aed6642985775c1c2fbdd5fa48aef1a694bec
ToniPerea <apepa31@gmail.com>
2022-06-30 19:52:14 +0200

    #12 Improve update token when change email in profile update

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -2,8 +2,9 @@ import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Deserialize, IJsonObject, Serialize} from 'dcerialize';
-import {map} from 'rxjs/operators';
+import {map, tap} from 'rxjs/operators';
 import {User} from "../models/user";
+import {JwtResponse} from "../models/jwt-response";
 
 
 @Injectable({
@@ -13,26 +14,29 @@ export class UserService {
     constructor(private http: HttpClient) {
     }
 
-    login(userData: User): Observable<User> {
-        return this.http.post<IJsonObject>('http://localhost:8080/login', Serialize(userData, () => User)).pipe(
-            map(user => Deserialize(user, () => User))
-        )
-    }
-
     register(userData: User): Observable<User> {
-      return this.http.post<IJsonObject>('http://localhost:8080/register', Serialize(userData, () => User)).pipe(
-          map(user => Deserialize(user, () => User))
-      )
-    }
-
-    put(userData: User): Observable<User> {
-        return this.http.put<IJsonObject>('http://localhost:8080/updateUser', Serialize(userData, () => User)).pipe(
+        return this.http.post<IJsonObject>('http://localhost:8080/register', Serialize(userData, () => User)).pipe(
             map(user => Deserialize(user, () => User))
         )
+    }
+
+    put(userEmail: string | null, userData: User): Observable<JwtResponse> {
+        return this.http.put<JwtResponse>(`http://localhost:8080/updateUser/${userEmail}`, Serialize(userData, () => User))
+            .pipe(tap(
+                (res: JwtResponse) => {
+                    if (res) {
+                        //guardar token
+                        localStorage.removeItem("ACCESS_TOKEN")
+                        localStorage.removeItem("EXPIRES_IN")
+                        localStorage.setItem("ACCESS_TOKEN", res.dataUser.accessToken);
+                        localStorage.setItem("EXPIRES_IN", res.dataUser.expiresIn);
+                    }
+                }
+            ))
     }
 
     get(userEmail: string): Observable<User> {
-      return this.http.get<IJsonObject>(`http://localhost:8080/getUserByEmail/${userEmail}`)
-          .pipe(map(user => Deserialize(user, () => User)))
+        return this.http.get<IJsonObject>(`http://localhost:8080/getUserByEmail/${userEmail}`)
+            .pipe(map(user => Deserialize(user, () => User)))
     }
 }
