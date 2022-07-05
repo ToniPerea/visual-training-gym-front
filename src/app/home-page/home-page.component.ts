81c64a63b95dfee98e68a29c735551e91a5d8f63
ToniPerea <apepa31@gmail.com>
2022-07-05 19:18:24 +0200

    #19 Add User List and Trainings List

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthService} from "../../services/auth.service";
 import {UserService} from "../../services/user.service";
+import {Roles} from "../../models/roles";
 
 @Component({
     selector: 'app-home-page',
@@ -10,6 +11,8 @@ import {UserService} from "../../services/user.service";
 export class HomePageComponent implements OnInit {
 
     role = ''
+
+    roleList = Roles
 
     constructor(private authService: AuthService, private userService: UserService) {
 
