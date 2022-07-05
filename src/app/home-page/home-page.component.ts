79da495195342c7cc8d4e859758f69dff05fe60e
ToniPerea <apepa31@gmail.com>
2022-07-05 16:19:27 +0200

    #19 Implementation of containers for trainer

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {AuthService} from "../../services/auth.service";
+import {UserService} from "../../services/user.service";
 
 @Component({
     selector: 'app-home-page',
@@ -7,10 +9,18 @@ import {Component, OnInit} from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
-    constructor() {
+    role = ''
+
+    constructor(private authService: AuthService, private userService: UserService) {
+
     }
 
     ngOnInit(): void {
+        this.userService.get(this.authService.getUserInfo().email).subscribe(user => {
+            this.role = user.role
+        })
+
+
     }
 
 }
